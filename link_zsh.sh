51f2a859dc25c307ff7d0fb74112ba97dc763a05
bhavnicksm <bhavnicksm@gmail.com>
2024-10-03 11:00:02 +0530

    Add zsh

---
+++ link_zsh.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash 
+
+# Path to your dotfiles and target config directory
+ZSHRC_ORIGIN_DIR="$(pwd)/zsh/.zshrc"
+ZSHRC_TARGET_DIR="$HOME/.zshrc"
+
+P10K_ORIGIN_DIR="$(pwd)/zsh/.p10k.zsh"
+P10K_TARGET_DIR="$HOME/.p10k.zsh"
+
+# Create symlink if not exists
+if [ ! -L "$ZSHRC_TARGET_DIR" ]; then
+    ln -s "$ZSHRC_ORIGIN_DIR" "$ZSHRC_TARGET_DIR"
+    echo "Linked $ZSHRC_ORIGIN_DIR to $ZSHRC_TARGET_DIR"
+else
+    echo "$ZSHRC_TARGET_DIR already exists as a symlink."
+fi
+
+if [ ! -L "$P10K_TARGET_DIR" ]; then
+    ln -s "$P10K_ORIGIN_DIR" "$P10K_TARGET_DIR"
+    echo "Linked $P10K_ORIGIN_DIR to $P10K_TARGET_DIR"
+else
+    echo "$P10K_TARGET_DIR already exists as a symlink."
+fi
